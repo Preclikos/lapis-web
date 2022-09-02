30e649d8360e0e0be01c36d02269d5aa036f2ab5
Jan Husak <jan.husak@ysoft.com>
2022-09-02 18:26:17 +0200

    remove bak files

--- packages/shared/data/menu.ts
+++ packages/shared/data/menu.ts
@@ -6,33 +6,6 @@ const menus: IMenu[] = [
         label: "Dashboard",
         url: "/",
         icon: "icon ion-ios-home-outline",
-        submenu: [
-            {
-                id: 11,
-                label: "Dashboard 01",
-                url: "/",
-            },
-            {
-                id: 12,
-                label: "Dashboard 02",
-                url: "/dashboard-two",
-            },
-            {
-                id: 13,
-                label: "Dashboard 03",
-                url: "/dashboard-three",
-            },
-            {
-                id: 14,
-                label: "Dashboard 04",
-                url: "/dashboard-four",
-            },
-            {
-                id: 15,
-                label: "Dashboard 05",
-                url: "/dashboard-five",
-            },
-        ],
     },
 ];
 
