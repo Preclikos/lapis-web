d684e319474b12ccd41edb1556a71721ee4e0f8d
Jan Husak <jan.husak@ysoft.com>
2022-09-13 14:41:11 +0200

    addt basic terms template

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Dashboard from "./pages/dashboard";
-import { importMessages, IntlProvider, LocaleMessages, SupportedLocales } from "./translations/intlHelpers";
-import { useEffect, useState } from "react";
-import SignIn from "./pages/signin";
-import SignOut from "./pages/signout";
-import Profile from "./pages/profile";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard';
+import {
+  importMessages,
+  IntlProvider,
+  LocaleMessages,
+  SupportedLocales,
+} from './translations/intlHelpers';
+import { useEffect, useState } from 'react';
+import SignIn from './pages/signin';
+import SignOut from './pages/signout';
+import Profile from './pages/profile';
+import TermsAndConditions from './pages/terms';
 
 function App() {
   const locale = navigator.languages[0].substring(0, 2) as SupportedLocales;
-  const [messages, setMessages] = useState<LocaleMessages | null>(null)
+  const [messages, setMessages] = useState<LocaleMessages | null>(null);
 
   useEffect(() => {
-    importMessages(locale).then(setMessages)
-  }, [locale])
+    importMessages(locale).then(setMessages);
+  }, [locale]);
 
   return messages ? (
     <IntlProvider locale={locale} messages={messages}>
       <Router>
         <Switch>
-          <Route exact path="/" component={Dashboard}  />
+          <Route exact path="/" component={Dashboard} />
 
-          <Route path="/profile" component={Profile}   />
-          <Route path="/profile/:id" component={Profile}   />
-          <Route path="/signin" component={SignIn}   />
-          <Route path="/signout" component={SignOut}   />
-          
+          <Route path="/profile" component={Profile} />
+          <Route path="/profile/:id" component={Profile} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signout" component={SignOut} />
+
+          <Route path="/terms" component={TermsAndConditions} />
         </Switch>
       </Router>
     </IntlProvider>
