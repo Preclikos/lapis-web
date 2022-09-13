95bfe1ca8e3c26917279e59ab42413d84f62e018
Jan Husak <jan.husak@ysoft.com>
2022-09-13 19:04:54 +0200

    dynamic keycloak

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,11 +1,11 @@
-import Keycloak from 'keycloak-js'
+import Keycloak from 'keycloak-js';
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = new Keycloak({
-    url: "https://login.lapis.report",
-    realm: "Lapis",
-    clientId: "lapis-web",
+  url: 'https://login.lapis.report',
+  realm: 'Lapis',
+  clientId: process.env.REACT_APP_CLIENT_ID,
 });
 
-export default keycloak
+export default keycloak;
--- src/pages/signout.tsx
+++ src/pages/signout.tsx
@@ -1,16 +1,17 @@
-import { useKeycloak } from "@react-keycloak/web";
-import { useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useKeycloak } from '@react-keycloak/web';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const SignOut = () => {
-    const router = useHistory();
-    const { keycloak } = useKeycloak();
-    useEffect(() =>
-    {
-        keycloak.logout({redirectUri: "https://lapis.report"}).then(() => router.push("/"))
-    });
+  const router = useHistory();
+  const { keycloak } = useKeycloak();
+  useEffect(() => {
+    keycloak
+      .logout({ redirectUri: process.env.REACT_APP_LOGOUT_REDIRECT })
+      .then(() => router.push('/'));
+  });
 
-    return (<>Loading</>);
+  return <>Loading</>;
 };
 
 export default SignOut;
